0d58093876efcad77627205ba88ae68f9286f2e7
Md. Maruf Al Hossain Prince <pmaruf42@gmail.com>
2019-11-03 11:19:44 +0600

    make http req and show output

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+const url = 'https://news.ycombinator.com/';
 
+axios.get(url)
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(err => {
+        console.log(err);
+    });

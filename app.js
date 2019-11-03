f2d6d189be638d46aca2d4e0d4843450cefed755
Md. Maruf Al Hossain Prince <pmaruf42@gmail.com>
2019-11-03 16:22:10 +0600

    scrap title and link of all articles

--- app.js
+++ app.js
@@ -5,8 +5,23 @@ const url = 'https://news.ycombinator.com/';
 
 axios.get(url)
     .then(response => {
-        console.log(response.data);
+        // const $ = cheerio.load(response.data);
+        let getData = html =>{
+            data = [];
+
+            const $ = cheerio.load(html);
+            $('table.itemlist tr td:nth-child(3)').each((i, elem) =>{
+                data.push({
+                    title: $(elem).text(),
+                    link: $(elem).find('a.storylink').attr('href')
+                });
+            });
+            console.log(data);
+        };
+
+        getData(response.data);
     })
     .catch(err => {
         console.log(err);
     });
+
